01331381f5c1c2f8b1a4553f1a286546ff6dfec9
Phillbert Tan <mrsilver512@users.noreply.github.com>
2018-09-12 19:56:48 +0800

    Packages Fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-montserrat';
+import "bootswatch/dist/yeti/bootstrap.min.css";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';

92d2054d1af1ef8d34454c372db44848959308cc
Phillbert Tan <mrsilver512@users.noreply.github.com>
2018-09-16 00:37:41 +0800

    Fixed font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'typeface-montserrat';
 import "bootswatch/dist/yeti/bootstrap.min.css";
 import './index.css';
 import App from './App';

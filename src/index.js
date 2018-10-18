1a3b833d32fc494b2e36fabee484299b89855ca7
Phillbert Tan <mrsilver512@users.noreply.github.com>
2018-10-18 17:23:58 +0800

    Testing prerendering

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import bugsnag from 'bugsnag-js';
 import createPlugin from 'bugsnag-react';
 import ReactGA from 'react-ga';
+import { render } from 'react-snapshot';
 import "./scss/Yeti.css";
 import "bootstrap/js/dist/collapse";
 const bugsnagClient = bugsnag(process.env.REACT_APP_BUGSNAG_TOKEN);
 const ErrorBoundary = bugsnagClient.use(createPlugin(React));
 
-ReactDOM.render(
+render(
 	<ErrorBoundary>
 		<App />
 	</ErrorBoundary>,
 	document.getElementById("root")
 );
+
 ReactGA.initialize("UA-45781991-6");
 ReactGA.pageview(window.location.pathname + window.location.search);
